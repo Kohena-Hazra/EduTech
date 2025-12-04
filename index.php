1c285539edfd33e36f5e63749abc4183392ef395
Kohena Hazra <kohenahazra@gmail.com>
2025-12-05 05:07:23 +0530

    updated

--- index.php
+++ index.php
@@ -54,7 +54,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['action']) && $_POST['a
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>EduTech</title>
-    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="View/css/styles.css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
 </head>
 <body>
@@ -170,6 +170,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['action']) && $_POST['a
         </div>
     </div>
 
-    <script src="script.js"></script>
+    <script src="View/script.js"></script>
 </body>
 </html>
